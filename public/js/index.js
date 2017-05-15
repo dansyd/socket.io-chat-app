dda69fce72efda124265ba03aa0f9fe58c3ebd0a
Daniele Peviani <daniele.peviani@gmail.com>
2017-05-15 16:11:26 +1000

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -23,30 +23,32 @@ socket.on('disconnect', function() {
   console.log('Disconnected from server');
 });
 
+const $locationButton = $('#send-location');
+const $messageTextBox = $('[name=message]');
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: $messageTextBox.val()
   }, function() {
-
+    $messageTextBox.val('');
   });
 });
-
-const $locationButton = $('#send-location');
 
 $locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser');
   }
-
+  $locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function(position) {
+    $locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
   }, function() {
-    alert('Unable to fetch location.')
+    alert('Unable to fetch location.');
+    $locationButton.removeAttr('disabled').text('Send location');
   })
 });
