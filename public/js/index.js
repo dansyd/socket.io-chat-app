e5a0432f51f452d6b0a14ea2f0fe794cbc6e264b
Daniele Peviani <daniele.peviani@gmail.com>
2017-05-15 14:07:56 +1000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Dani',
-    text: 'Ok, Ill be home soon ;)'
-  });
-
 });
 
 socket.on('newMessage', function(message) {
